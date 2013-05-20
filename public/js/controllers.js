89eefa31608b05d1043ae4edfd4df5fe516cd489
Johannes Haseitl <johannes@undpaul.de>
2013-05-20 18:14:20 +0200

    Some markup changes to prettify

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -3,12 +3,34 @@
 /* Controllers */
 
 /**
+ * Controller for the whole html content.
+ *
+ * @param $scope
+ * @param $location
+ * @constructor
+ */
+function AppCtrl($scope, $location) {
+  $scope.getClass = function(path) {
+    if ($location.path().indexOf(path) == 0) {
+      return "active";
+    }
+    else {
+      return "";
+    }
+  }
+}
+AppCtrl.$inject = ['$scope', '$location'];
+
+
+/**
  * Controller for drivers overview.
  *
  * @param $scope
  * @param $http
  */
 function driversCtrl($scope, $http) {
+  $scope.drivers = [];
+
   $http({method: 'GET', url: '/api/drivers'}).
     success(function(data, status, headers, config) {
       $scope.drivers = data;
@@ -33,3 +55,4 @@ function settingsCtrl() {
 
 }
 settingsCtrl.$inject = [];
+
