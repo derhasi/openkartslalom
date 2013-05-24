3df6e67db26e2ba6afe5897e2c630388ffd78a43
Johannes Haseitl <johannes@undpaul.de>
2013-05-24 16:18:09 +0200

    Adding basic ng-translate capability.

--- public/js/app.js
+++ public/js/app.js
@@ -2,15 +2,25 @@
 
 
 // Declare app level module which depends on filters, and services
-var openKS = angular.module('openKS', ['ngResource', 'openKS.filters', 'openKS.services', 'openKS.directives']);
+var openKS = angular.module('openKS', ['ngResource', 'openKS.filters', 'openKS.services', 'openKS.directives', 'pascalprecht.translate']);
 
-openKS.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+openKS.config(['$routeProvider', '$locationProvider', '$translateProvider', function($routeProvider, $locationProvider, $translateProvider) {
+  // Routes defined.
   $routeProvider.when('/home', {templateUrl: 'partials/home', controller: HomeCtrl});
   $routeProvider.when('/drivers', {templateUrl: 'partials/drivers', controller: driversCtrl});
   $routeProvider.when('/drivers/:id/edit', {templateUrl: 'partials/driver-edit', controller: driversEditCtrl});
   $routeProvider.when('/settings', {templateUrl: 'partials/settings', controller: settingsCtrl});
   $routeProvider.otherwise({redirectTo: '/home'});
   $locationProvider.html5Mode(true);
+
+  // Configure translation service.
+  $translateProvider.translations({
+    "Home": "Startseite",
+    "Drivers": "Fahrer",
+    "Settings": "Einstellungen",
+    "Save settings": "Einstellungen speichern",
+    "Title": "Titel"
+  });
 }]);
 
 // @todo minify support?
