22c44627508366cdea692c826b37c05e9bd82521
Johannes Haseitl <johannes@undpaul.de>
2013-05-26 19:35:13 +0200

    Api callbacks for result CRUD.

---
+++ models/result.js
@@ -0,0 +1,135 @@
+var fs = require('fs');
+var config = require('./config');
+
+const resultCurrentJSONPath = "data/current/results.json";
+
+/**
+ * Get all results in an array.
+ *
+ * @param callback
+ *  Provides:
+ *  - err: error object
+ *  - Array: array of the result objects
+ */
+exports.list = function(callback) {
+
+  // If there is a driver json, we use it directly.
+  if (fs.existsSync(resultCurrentJSONPath)) {
+    console.log('drivers.json found in data/current.');
+    fs.readFile(resultCurrentJSONPath, function(err, content) {
+      if (err) {
+        console.log('Error on reading %s: ', resultCurrentJSONPath, err);
+      }
+      callback(null, JSON.parse(content));
+    });
+  }
+  // Otherwise we have simply no data.
+  else {
+    callback(null, []);
+  }
+}
+
+/**
+ * Get a driver object for the given id.
+ *
+ * @param id
+ *   ID of the driver
+ * @param callback
+ *  Provides:
+ *  - err: error object
+ *  - Object: a driver objectw
+ */
+exports.get = function(id, callback) {
+  this.list(function (err, results) {
+    if (err) {
+      callback(err);
+    }
+    // Run through the list of drivers
+    for (var key in results) {
+      if (results[key].__id == id) {
+        callback(null, results[key]);
+        return;
+      }
+    }
+
+    callback(new Error('Result not found'));
+  });
+}
+
+/**
+ * Get a new empty result.
+ *
+ * @param callback
+ *  Provides:
+ *  - err: error object
+ *  - Object: a result object
+ */
+exports.new = function(callback) {
+  this.list(function (err, results) {
+    if (err) {
+      callback(err);
+    }
+
+    // Get the next id by running through all ids.
+    var max = 0;
+    for (var i in results) {
+      if (results[i].__id && results[i].__id > max) {
+        max = results[i].__id;
+      }
+    }
+
+    // Simply pass an empty object with a N00000 id.
+    callback(null, {
+      "__id": max + 1
+    });
+  });
+}
+
+
+/**
+ * Write a result object back to the results json.
+ *
+ * @param obj
+ *   The result object (new or to update).
+ * @param callback
+ *  Provides:
+ *  - err: error object
+ *  - Object: a result object
+ */
+exports.write = function(obj, callback) {
+  this.list(function (err, results) {
+    if (err) {
+      callback(err);
+    }
+
+    if (obj.__id == undefined) {
+      callback(new Error('No __id given for the object.'));
+      return;
+    }
+
+    // Update revision by 1.
+    obj.v += 1;
+    // @todo: add created and changed attributes
+
+    var found = false;
+    // Run through the list of drivers, and replace the object having the same
+    // __id with the new one.
+    for (var key in results) {
+      if (results[key].__id == obj.__id) {
+        results[key] = obj;
+        found = true;
+        break;
+      }
+    }
+
+    // If there was no existing entry, simply add id to the end of the list.
+    if (!found) {
+      results.push(obj);
+    }
+
+    // And then we will write the file back.
+    fs.writeFile(resultCurrentJSONPath, JSON.stringify(results, null, " "), function(err) {
+      callback(err, obj);
+    });
+  });
+}
--- routes/api.js
+++ routes/api.js
@@ -6,6 +6,7 @@ var fs = require('fs');
 var csv = require('csv');
 var config = require('../models/config');
 var driver = require('../models/driver');
+var result = require('../models/result');
 
 /**
  * Route callback to show all drivers as JSON.
@@ -15,11 +16,11 @@ var driver = require('../models/driver');
  */
 exports.getDrivers = function (req, res) {
   // Output the list of drivers.
-  driver.list(function (err, drivers) {
+  driver.list(function (err, drvs) {
     if (err) {
       console.log(err);
     }
-    res.json(drivers);
+    res.json(drvs);
   });
 }
 
@@ -30,16 +31,16 @@ exports.getDrivers = function (req, res) {
  * @param res
  */
 exports.getDriver = function(req, res) {
-  driver.get(req.params.driverId, function (err, driver) {
+  driver.get(req.params.driverId, function (err, drv) {
     if (err) {
       console.log('Error retrieving driver: ', err);
       res.status(404).send('No driver found');
     }
-    else if (!driver) {
+    else if (!drv) {
       res.status(404).send('No driver found');
     }
     else {
-      res.json(driver);
+      res.json(drv);
     }
   });
 }
@@ -51,11 +52,11 @@ exports.getDriver = function(req, res) {
  * @param res
  */
 exports.newDriver = function(req, res) {
-  driver.new(function (err, driver) {
+  driver.new(function (err, drv) {
     if (err) {
       console.log('Error retrieving new driver: ', err);
     }
-    res.json(driver);
+    res.json(drv);
   });
 }
 
@@ -75,11 +76,11 @@ exports.saveDriver = function(req, res) {
     res.status('406').send('Param and object id do not match');
   }
   else {
-    driver.write(req.body, function(err, driver) {
+    driver.write(req.body, function(err, drv) {
       if (err) {
         console.log('Error saving driver: ', req.body);
       }
-      res.json(driver);
+      res.json(drv);
     });
   }
 }
@@ -116,5 +117,83 @@ exports.setConfig = function (req, res) {
   // We do not have any data to save.
   else {
     res.json(config.load());
+  }
+}
+
+
+/**
+ * Route callback to show all drivers as JSON.
+ *
+ * @param req
+ * @param res
+ */
+exports.getResults = function (req, res) {
+  // Output the list of results.
+  result.list(function (err, rslts) {
+    if (err) {
+      console.log(err);
+    }
+    res.json(rslts);
+  });
+}
+
+/**
+ * Endpoint for getting a driver object from the stored json.
+ *
+ * @param req
+ * @param res
+ */
+exports.getResult = function(req, res) {
+  result.get(req.params.driverId, function (err, rslt) {
+    if (err) {
+      console.log('Error retrieving result: ', err);
+      res.status(404).send('No result found');
+    }
+    else if (!rslt) {
+      res.status(404).send('No result found');
+    }
+    else {
+      res.json(rslt);
+    }
+  });
+}
+
+/**
+ * Endpoint callback to get an object with a new id.
+ *
+ * @param req
+ * @param res
+ */
+exports.newResult = function(req, res) {
+  result.new(function (err, rslt) {
+    if (err) {
+      console.log('Error retrieving new driver: ', err);
+    }
+    res.json(rslt);
+  });
+}
+
+/**
+ * Endpoint callback for saving a driver object.
+ * @param req
+ * @param res
+ */
+exports.saveResult = function(req, res) {
+  if (!req.body) {
+    res.status('406').send('No object given.');
+  }
+  else if (req.body.__id == undefined) {
+    res.status('406').send('No object __id given');
+  }
+  else if (req.body.__id != req.params.id) {
+    res.status('406').send('Param and object id do not match');
+  }
+  else {
+    result.write(req.body, function(err, rslt) {
+      if (err) {
+        console.log('Error saving driver: ', req.body);
+      }
+      res.json(rslt);
+    });
   }
 }
