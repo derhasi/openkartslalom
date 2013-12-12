ff41c8306653667a394d1dab5d1309a2733cb6c1
Johannes Haseitl <johannes@undpaul.de>
2013-12-12 21:17:30 +0100

    Added data store results.
    refs #5

--- app/js/services.js
+++ app/js/services.js
@@ -12,6 +12,7 @@ openKS.factory('openKSDatabase', function openKSDatabaseFactory() {
 
   var dbInitted = 0;
 
+  // Provides data store for drivers.
   var driverDB = new IDBStore({
     storeName: 'drivers',
     storePrefix: 'openKS-',
@@ -29,10 +30,27 @@ openKS.factory('openKSDatabase', function openKSDatabaseFactory() {
     ]
   });
 
+  // Adds data store for results.
+  var resultDB = new IDBStore({
+    storeName: 'results',
+    storePrefix: 'openKS-',
+    dbVersion: 1,
+    keyPath: 'id',
+    autoIncrement: true,
+    onStoreReady: function(){
+      dbInitted++;
+    },
+    indexes: [
+      { name: 'driverID' },
+      { name: 'status' }
+    ]
+  });
+
   return {
     "driverDB": driverDB,
+    "resultDB": resultDB,
     "dbReady": function () {
-      return dbInitted >= 1;
+      return dbInitted >= 2;
     }
   };
 });
