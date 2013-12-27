5cbc8d0ef1595b554cfd74a58854f48c5d8172fb
Johannes Haseitl <johannes@undpaul.de>
2013-12-27 20:36:13 +0100

    Implemented basic edit result item form
    fixes #7

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -201,7 +201,46 @@ openKS.controller('DriverFormCtrl', ['$scope', 'openKSDriver', 'openKSNavigation
 /**
  * Controller for result overview.
  */
-openKS.controller('ResultsCtrl', [function() {}]);
+openKS.controller('ResultsCtrl', ['$scope', 'openKSResult', 'openKSNavigation', function($scope, openKSResult, nav) {
+  $scope.loading = true;
+
+  $scope.results = [];
+
+  /**
+   * Loads the all drivers asynchronously.
+   */
+  $scope.loadResults = function() {
+    // We mark the scope, that we are loading the drivers list again.
+    $scope.loading = true;
+
+    openKSResult.loadAll(
+      function(results) {
+        // Assign the fetched result items to our scope variable.
+        $scope.results = results;
+        // As we finished, we can unflag the loading state.
+        $scope.loading = false;
+        // ... as the call is async, we need to tell our scope that it has been
+        // updated.
+        $scope.$apply();
+      }
+    );
+  }
+
+  // Initially load the results on controller init.
+  $scope.loadResults();
+
+  /**
+   * Wrapper for navigating to the edit view of the given entry.
+   *
+   * @param id
+   */
+  $scope.editView = function (id) {
+    nav.setView('resultEdit', [id], function () {
+      $scope.$apply();
+    });
+  }
+
+}]);
 
 /**
  * Controller for the result form.
