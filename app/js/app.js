fe43b916fc809936e9411d352794c7c3277315c1
Johannes Haseitl <johannes@undpaul.de>
2014-07-13 00:07:21 +0200

    (#15) Replace basic navigation with ui.rooter
    - @todo: reimplement history

--- app/js/app.js
+++ app/js/app.js
@@ -2,5 +2,94 @@
 
 // Declare app level module which depends on filters, and services
 var openKS = angular.module('openKS', [
-  'ui.bootstrap', 'openKS.nav', 'openKS.home'
+  'ui.bootstrap', 'ui.router', 'openKS.nav', 'openKS.home'
 ]);
+
+// Some basic configuration for our app.
+openKS.config(function($stateProvider) {
+
+  // For making ui-view work, we use the stateProvider. As we are in a chrome
+  // packaged App, we cannot use a URL or location provider.
+  $stateProvider
+    .state('home', {
+      templateUrl: 'views/home.html',
+      data: {
+        title: 'Home',
+        iconClass: "fa fa-home"
+      }
+    })
+    .state('results', {
+      templateUrl: 'views/results.html',
+      data: {
+        title: 'Results',
+        iconClass: "fa fa-trophy",
+        breadcrumbParent: 'home'
+      }
+    })
+    .state('drivers', {
+      templateUrl: 'views/drivers.html',
+      data: {
+        title: 'Drivers',
+        iconClass: "fa fa-users",
+        breadcrumbParent: 'home'
+      }
+    })
+    .state('settings', {
+      templateUrl: 'views/settings.html',
+      data: {
+        title: 'Settings',
+        breadcrumbParent: 'home',
+        iconClass: 'fa fa-cog'
+      }
+    })
+    .state('info', {
+      templateUrl: 'views/info.html',
+      data: {
+        title: 'Info',
+        breadcrumbParent: 'home',
+        iconClass: 'fa fa-info'
+      }
+    })
+    .state('driverAdd', {
+      templateUrl: 'views/driver-form.html',
+      data: {
+        title: 'Add driver',
+        breadcrumbParent: 'drivers',
+        iconClass: 'fa fa-plus'
+      }
+    })
+    .state('driverEdit', {
+      templateUrl: 'views/driver-form.html',
+      data: {
+        title: 'Edit driver',
+        breadcrumbParent: 'drivers',
+        iconClass: 'fa fa-edit'
+      }
+    })
+    .state('resultAdd', {
+      templateUrl: 'views/result-form.html',
+      data: {
+        title: 'Add result',
+          breadcrumbParent: 'results',
+        iconClass: 'fa fa-clock-o'
+      }
+    })
+    .state('resultEdit', {
+      templateUrl: 'views/result-form.html',
+      data: {
+        title: 'Edit result',
+        breadcrumbParent: 'results',
+        iconClass: 'fa fa-edit'
+      }
+    })
+    .state('timing', {
+      templateUrl: 'views/timing.html',
+      data: {
+        title: 'Timing',
+        breadcrumbParent: 'home',
+        iconClass: 'fa fa-clock-o'
+      }
+    });
+
+
+});
